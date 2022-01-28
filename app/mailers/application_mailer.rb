9f54d9132ec7c0f383b205db130e8a554aa5d714
Chris Vilches <felovilches@gmail.com>
2022-01-29 01:00:21 +0900

    layout mailer (fix)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,5 @@ class ApplicationMailer < ActionMailer::Base
   default from: ENV.fetch('NO_REPLY_ADDRESS')
   default reply_to: ENV.fetch('NO_REPLY_ADDRESS')
   default content_type: 'text/html'
+  layout 'mailer'
 end
